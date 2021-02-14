77fea62add50910c5b857e14fac2f7796c285937
jacktrinh123 <hoangtq.iuh@gmail.com>
2021-02-14 17:12:44 +0700

    add  admin template, add contact form, categories form, products form

---
+++ admin/adminindex.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<?php 
+include("adminpartials/head.php");
+?>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <?php 
+  include("adminpartials/header.php");
+  include("adminpartials/aside.php");
+  ?>
+  <!-- Left side column. contains the logo and sidebar -->
+  
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+        <small>Control panel</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <!-- Small boxes (Stat box) -->
+      <h1>Hello</h1>
+      <!-- /.row (main row) -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <?php 
+  include("adminpartials/footer.php");
+  ?>
+</body>
+</html>
---
+++ admin/cathandler.php
@@ -0,0 +1,7 @@
+<?php 
+include("../partials/connect.php");
+$name = $_POST["name"];
+
+$sql = "INSERT INTO categories(name) VALUES('$name')";
+$connect->query($sql)
+?>
