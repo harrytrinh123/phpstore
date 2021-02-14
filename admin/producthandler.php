1b1608fb257032186cfcb27e10cd5a84f3ff2dee
jacktrinh123 <hoangtq.iuh@gmail.com>
2021-02-14 22:53:11 +0700

    saving product data

---
+++ admin/producthandler.php
@@ -0,0 +1,20 @@
+<?php 
+include("../partials/connect.php");
+$name = $_POST["name"];
+$price = $_POST["price"];
+$description = $_POST["description"];
+$category = $_POST["category"];
+
+$target = "../uploads/";
+$file_path = $target.basename($_FILES["file"]["name"]); //../uploads/pro_A61098_1.jpg
+$file_name = $_FILES["file"]["name"]; //pro_A61098_1.jpg
+$file_tmp = $_FILES["file"]["tmp_name"];
+$file_store = "../uploads/".$file_name;
+
+echo $file_tmp;
+
+move_uploaded_file($file_tmp, $file_store);
+
+$sql = "INSERT INTO products(name, price, picture, description, category_id) VALUES('$name', '$price', '$file_path', '$description', '$category')";
+$connect->query($sql)
+?>
--- admin/products.php
+++ admin/products.php
@@ -33,30 +33,30 @@ include("adminpartials/head.php");
         <!-- Small boxes (Stat box) -->
         <div class="col-sm-3"></div>
         <div class="col-sm-6">
-          <form role="form">
+          <form role="form" action="producthandler.php" method="POST" enctype="multipart/form-data">
             <h1>Products</h1>
             <div class="box-body">
               <div class="form-group">
                 <label for="name">Name</label>
-                <input type="text" class="form-control" id="name" placeholder="Enter Product Name">
+                <input type="text" class="form-control" id="name" placeholder="Enter Product Name" name="name">
               </div>
               <div class="form-group">
                 <label for="price">Price</label>
-                <input type="text" class="form-control" id="price" placeholder="Price">
+                <input type="text" class="form-control" id="price" placeholder="Price" name="price">
               </div>
               <div class="form-group">
                 <label for="picture">File input</label>
-                <input type="file" id="picture">
+                <input type="file" id="picture" name="file">
               </div>
               <div class="form-group">
                 <label for="description">Description</label>
-                <textarea class="form-control" id="description" cols="30" rows="10" placeholder="Enter description"></textarea>
+                <textarea class="form-control" id="description" cols="30" rows="10" placeholder="Enter description" name="description"></textarea>
               </div>
               <div class="form-group">
                 <label for="category">Category</label>
-                <select name="" id="category">
-                  <option value="">Shirts</option>
-                  <option value="">Pants</option>
+                <select id="category" name="category">
+                  <option value="1">Shoes</option>
+                  <option value="2">Pants</option>
                 </select>
               </div>
             </div>
