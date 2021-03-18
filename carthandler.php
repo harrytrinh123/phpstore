4e495e927ef3682d31aab84cb0301ae34312fb0b
jacktrinh123 <hoangtq.iuh@gmail.com>
2021-03-18 18:57:22 +0700

    initial cart

---
+++ carthandler.php
@@ -0,0 +1,22 @@
+<?php 
+session_start();
+if(isset($_SESSION['cart'])) {
+    $chker=array_column($_SESSION['cart'], 'item_name');
+    if(in_array($_GET['cart_name'], $chker)) {
+        echo "<script>
+            alert('Product is alread in the cart');
+            window.location.href='product.php';
+        </script>";
+    }
+    $count = count($_SESSION['cart']);
+    $_SESSION['cart'][$count] = array('item_id'=>$_GET['cart_id'], 'item_name'=>$_GET['cart_name'], 'item_price'=>$_GET['cart_price']);
+    echo "<script>
+            alert('Product Added');
+            window.location.href='product.php';
+        </script>";
+} else {
+    $_SESSION['cart'][0] = array('item_id'=>$_GET['cart_id'], 'item_name'=>$_GET['cart_name'], 'item_price'=>$_GET['cart_price']);
+
+}
+print_r($_SESSION['cart']);
+?>
